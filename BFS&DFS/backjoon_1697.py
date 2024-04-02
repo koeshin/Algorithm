f7e3855d4ccd5b7aa468299ffa2c95e715be3325
koeshin <tlswodnjs17@gmail.com>
2024-04-02 16:36:01 +0900

    1697 플음

--- BFS&DFS/backjoon_1697.py
+++ BFS&DFS/backjoon_1697.py
@@ -12,3 +12,84 @@
 '''
 
 
+'''import sys
+from collections import deque
+N,K=list(map(int,sys.stdin.readline().split()))
+
+    
+visited=[0]*(100001)    
+
+def bfs(V,K):
+    
+    
+    
+    
+    # print(visited)
+    tmp=deque()    
+    tmp.append(V)
+    # print("tmp: ",tmp)
+    visited[V]=1
+    while(tmp):
+        curr=tmp.popleft()
+        
+        if curr==K:
+            # return visited[K]-1
+            print(visited[K]-1)
+            
+        # print("curr:",curr) 
+
+        if curr<K:
+            curr1=curr+1
+            if  visited[curr1]==0: 
+                visited[curr1]=visited[curr]+1
+                tmp.append(curr1)
+        if 2*curr<=2*K:
+            curr2=curr*2
+            if  visited[curr2]==0: 
+                visited[curr2]=visited[curr]+1
+                tmp.append(curr2)
+        if 1<=curr:
+            curr3=curr-1
+            # print("curr3:",curr3)
+            if  visited[curr3]==0: 
+                visited[curr3]=visited[curr]+1
+                # print("visited:",visited[curr3])
+                tmp.append(curr3)
+            
+    return
+
+
+bfs(N,K)
+'''
+
+import sys
+from collections import deque
+
+def bfs2(v):
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if v == k:
+            return array[v]
+        for next_v in (v-1, v+1, 2*v):
+            if 0 <= next_v < MAX and not array[next_v]:
+                array[next_v] = array[v] + 1
+                q.append(next_v)
+
+
+MAX = 100001
+n, k = list(map(int,sys.stdin.readline().split()))
+array = [0] * MAX
+res2=bfs2(n)
+
+# for i in range(100000):
+#     for j in range(100000):
+#         N,K = i,j
+#         n, k = N,K
+#         array = [0] * MAX
+#         res2=bfs2(n)
+#         res=bfs(N,K)
+        
+#         if res != res2:
+#             print("eror")
+#             print("i,j:",i,j)
