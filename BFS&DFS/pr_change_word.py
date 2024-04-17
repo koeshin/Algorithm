d013be005b8963b4914d1157b25a9f21919aee32
koeshin <tlswodnjs17@gmail.com>
2024-04-17 13:13:38 +0900

    여행경로&단어 변환 문제 주석 추가

--- BFS&DFS/pr_change_word.py
+++ BFS&DFS/pr_change_word.py
@@ -1,5 +1,5 @@
 
-def check_diff(begin,word):
+def check_diff(begin,word): # 현재 단어와 다른 단어의 개수 리턴
     res=0
     for i in range(len(begin)):
         if begin[i]!=word[i]:
@@ -9,18 +9,18 @@ def check_diff(begin,word):
 def dfs(begin, target, words,visited):
     global l
     res=0
-    visited_tmp=visited.copy()
-    visited_tmp[words.index(begin)]=1 
-    if begin==target:
+    visited_tmp=visited.copy()   # visited 복사
+    visited_tmp[words.index(begin)]=1  # words리스트에서 현재 단어의 인덱스 가져와서 방문 처리
+    if begin==target: # 현재 단어가 타겟하고 같으면 이동 횟수 리턴
 
-        res=sum(visited_tmp)
+        res=sum(visited_tmp)  # 총 이동 횟수 계산
         l.append(res)
         
     for i in range(len(words)):
     
-        if visited_tmp[i]==0:
+        if visited_tmp[i]==0:   # 방문하지 않은  단어 이고
             check_res=check_diff(begin,words[i])
-            if check_res==1:
+            if check_res==1:    # 다른 글자 수가 한개이면 dfs
                 dfs(words[i],target,words,visited_tmp)
                 
     return   
@@ -28,15 +28,15 @@ def solution(begin, target, words):
     answer = 0
     global l
     l=[]
-    if target not in words:
+    if target not in words:   # target 단어가 words에 없을 경우, 리턴
         answer=0
         return answer
-    for i in range(len(words)):
-        visited=[0]*len(words)
+    for i in range(len(words)):   
+        visited=[0]*len(words)   # 방문한 단어 리스트
         
-        check_res=check_diff(begin,words[i])
-        if check_res==1:
-            print(words[i])
+        check_res=check_diff(begin,words[i]) # 다른 글자 수 확인
+        if check_res==1:  #1글자만 다르면 dfs
+            
             dfs(words[i],target,words,visited)
             
     answer=min(l)
--- BFS&DFS/pr_travel_route.py
+++ BFS&DFS/pr_travel_route.py
@@ -1,34 +1,34 @@
 
 
-def dfs(start,tickets,course,visited,idx):
+def dfs(start,tickets,course,visited,idx): # 현재위치, 티켓 리스트, 지금까지 코스, 방문기록, 이전에 사용한 티켓의 인덱스 입력
     global answer
-    curr=start
-    course_copy=course.copy()
-    visited_copy=visited.copy()
-    course_copy.append(curr)
+    curr=start    # 현재 위치
+    course_copy=course.copy()  # 이동경로 복사
+    visited_copy=visited.copy() # 방문 기록 복사
+    course_copy.append(curr)    # 이동경로에 현재 위치 추가
 
-    if len(course_copy)==len(tickets)+1:
+    if len(course_copy)==len(tickets)+1:   # 총 이동한 도시는 티켓 수 +1 => 모든 곳을 다 갔으면 리터
         answer.append(course_copy)
         
         return 
     
     for i in range(len(tickets)):
-        if tickets[i][0]==curr and visited_copy[i]==0:
-            visited_copy[i]=1
-            dfs(tickets[i][1],tickets,course_copy,visited_copy,i)
-        if i==len(tickets)-1:
-            visited[idx]=0
+        if tickets[i][0]==curr and visited_copy[i]==0:  # 티켓의 출발지가 현재 위치이고 사용안한 티켓일 때,
+            visited_copy[i]=1    #  방문 처리
+            dfs(tickets[i][1],tickets,course_copy,visited_copy,i) # 재귀 dfs
+        if i==len(tickets)-1:   # 만약 티켓의 마지막 인덱스까지 갔지만 이동하지 못하고 갇히는 경우
+            visited[idx]=0      # 이전에 사용한 티켓의 방문하지 않음으로 수정 -> 갇히면 다른 경우의 수 고려
     return 
 def solution(tickets):
     global answer
     answer = []
-    v=[0]*len(tickets)
+    v=[0]*len(tickets)   # 사용한 티켓을 저장하는 리스트 
     for i in range(len(tickets)):
-        course=["ICN"]
+        course=["ICN"]          # 시작은 무조건 인천
         visited=v.copy()
-        if tickets[i][0]=="ICN":
-            visited[i]=1
-            dfs(tickets[i][1],tickets,course,visited,i)
+        if tickets[i][0]=="ICN":  # 출발지가 인천인 곳 찾기
+            visited[i]=1          # 티켓 사용으로 처리
+            dfs(tickets[i][1],tickets,course,visited,i) # dfs
     answer.sort()
     answer=answer[0]
     return answer
