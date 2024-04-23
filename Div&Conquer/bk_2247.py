2ee39058be5ecd7763da29c39e49e5a5cd4dc3ba
koeshin <tlswodnjs17@gmail.com>
2024-04-23 17:05:52 +0900

    별그리기 정답코드(풀지못함)

--- Div&Conquer/bk_2247.py
+++ Div&Conquer/bk_2247.py
@@ -3,46 +3,26 @@ import sys
 
 global N
 N=int(sys.stdin.readline().strip())
-def Print_3_star(idx,n,long):   # idx= 맨위나 중간을 그릴지, n= 현재 3의 거듭제곱 값, long=N//n
-    l=n//3
-    if l==1:
-        if idx== 'top':
-            str='***'*long
 
-            stars=str
-        if idx=="mid":
-            str="* *"*long
-        
-            stars=str
-
-    else:
-        if idx== 'top':
-            str='*'*l
-            blank=' '*l
-            stars=(str+blank+str)*long
-        if idx=="mid":
-            str="* *"*(l//3)
-            blank=' '*l
-            stars=(str+blank+str)*long
-    print(stars)
-    print(len(stars))
-    return
-
-def solution(n):
-    global N
-    k=0
-    while n>1:
-        n=n//3
-        k+=1
+def make_star(n):
     
-    for i in range(k):
-        num=3**(i)
-        long=N//num
-        Print_3_star("top",num,long)
-        Print_3_star("mid",num,long)
-        Print_3_star("top",num,long)
-        
-        
-        
+    if n==1:
+        return ['*']
+    star=make_star(n//3)
+    
+    stars=[]
+    
+    for a in star:
+        stars.append(a*3)  # 첫 번째 층 이전 결과를 3개씩 복사해서 stars에 추가
+    for b in star:
+        stars.append(b+" "*(n//3)+b) # 두 번째 층 이전 결과에 빈 공간을 추가해서 stars에 추가
+    for c in star:
+        stars.append(c*3)  # 첫번째 층과 동일
 
-Print_3_star('top',27,3)
+    return stars
+
+
+
+stars=make_star(N)
+for star in stars:
+    print(star)
