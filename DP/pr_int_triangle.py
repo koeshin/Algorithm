6e6f2f2686fc41725ba069dc1cd121b8ef38583a
koeshin <tlswodnjs17@gmail.com>
2024-04-26 11:13:24 +0900

    프로그래머스 정수 삼각형

---
+++ DP/pr_int_triangle.py
@@ -0,0 +1,15 @@
+
+
+def solution(triangle):
+
+    
+    
+    for i in range(len(triangle)-2,-1,-1):
+        for j in range(len(triangle[i])):
+            tmp=max(triangle[i+1][j],triangle[i+1][j+1])  # 이전 단계에서 맥스값 찾기
+            triangle[i][j]=triangle[i][j]+tmp #현재 단계 값 업데이트 
+    
+    return triangle[0][0]
+            
+
+
