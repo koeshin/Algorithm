1fcfffef13a6dd2839c4ae66c0cb85af0a9cd7b3
koeshin <tlswodnjs17@gmail.com>
2024-04-29 13:05:25 +0900

    프로그래머스 석유 시추 문제해결

--- BFS&DFS/pr_oil_pulling.py
+++ BFS&DFS/pr_oil_pulling.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-from collections import deque
+
 
 
 def BFS(maps,start,visited):
@@ -23,8 +23,7 @@ def BFS(maps,start,visited):
             cy=y+dy[i]
             
             if 0<= cx < len(maps) and 0<= cy < len(maps[0]):
-                if visited[cx][cy]==False and maps[cx][cy]!=0:
-                    # maps[cx][cy]=maps[x][y]+1
+                if visited[cx][cy]==False and maps[cx][cy]==1:
                     visited[cx][cy]=True
                     queue.append((cx,cy))
                     # end=(cx,cy)
@@ -59,42 +58,58 @@ def solution(land):
 # land=[[0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 1, 0, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]]
 # print(solution(land))
                     
-######################## 수정 중인 코드#####################
-def solution2(land):   # 이  코드는 for문이 너무 많아서 시간 초과
+######################## 정답 코드#####################
+
+
+def BFS2(maps,start,visited):
+    
+    dx=[1,-1,0,0]
+    dy=[0,0,1,-1]
+    queue=deque()
+    queue.append(start)
+    oils=1
+    min_y=1000
+    max_y=0
+
+    
+    while queue:
+        now=queue.popleft()
+        x,y=now
+        min_y=min(min_y,y)   # 석유가 들어있는 최소 컬럼 값 
+        max_y=max(max_y,y)   # 석유가 들어있는 최대 컬럼값
+        visited[x][y]=True
+        for i in range(4):
+            cx=x+dx[i]
+            cy=y+dy[i]
+            
+            if 0<= cx < len(maps) and 0<= cy < len(maps[0]):
+                if visited[cx][cy]==False and maps[cx][cy]==1: # map[x][y]!=0 보다는 ==1이 더 효율적인 방법
+                    visited[cx][cy]=True
+                    queue.append((cx,cy))
+                    # end=(cx,cy)
+                    oils+=1
+                    
+    
+    return oils,min_y,max_y
+
+def solution2(land):   
     colums=len(land[0])
     rows=len(land)
-    dict={}
-    tmp_land=land.copy()
-    MAX=0
-    for colum in range(colums):
-        print("dict",dict)
-        results=[]
-        starts=[]
-        visited=[[False]*colums for _ in range(rows)]
+    result=[0]*colums   # 컬럼마다 시추한 석유 양을 담을 리스트
+    visited=[[False]*colums for i in range(rows)] 
+    for i in range(colums):
+        for j in range(rows):
+            if land[j][i]==1 and visited[j][i]==False:   # 석유가 있고 방문하지 않았을 경우만 BFS
+                oils,min_y,max_y=BFS2(land,(j,i),visited)
+               
+                for idx in range(min_y,max_y+1):   # 컬러마다 시추한 석유양 더해주기
+                    result[idx]+=oils
 
-        for row in range(rows):
-            if land[row][colum]>=1:
-                starts.append((row,colum))
-        check={}    
-        print("starts:",starts)  
-        for start in starts:
-            print("check:",check)   
-            x,y=start
-            if tmp_land[x][y] !=1:
-                if tmp_land[x][y] not in check:
-                    print(tmp_land[x][y])
-                    results.append(dict[tmp_land[x][y]])
-                    check[tmp_land[x][y]]=result
-            else:
-                result=BFS(tmp_land,start,visited)
-                dict[colum+2]=result
-                check[colum+2]=result
-                results.append(result)
-        print("results:",results)
-        if sum(results)>MAX:
-            MAX=sum(results)
+     
+
+
     
-    return MAX
+    return max(result)
 land=[[1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 1, 0], [1, 0, 0, 0], [1, 1, 1, 1]]
 
 land=[[1, 1, 1, 1, 0],
@@ -103,11 +118,3 @@ land=[[1, 1, 1, 1, 0],
 [1, 0, 0, 1, 0],
 [1, 1, 0, 1, 0]]
 print(solution2(land))
-
-# print(solution([[0, 0, 0, 1, 1, 1, 1, 1],
-#           [0, 0, 0, 0, 0, 0, 0, 1],
-#            [1, 1, 1, 1, 1, 1, 1, 1],
-#            [1, 1, 1, 0, 0, 0, 0, 0],
-#            [1, 1, 1, 0, 0, 0, 1, 1]])) 
-                    
-            
