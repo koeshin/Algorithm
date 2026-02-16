4ff040ca243efe8d7181ab75d5ab6114ab7a4318
koeshin <tlswodnjs17@gmail.com>
2026-02-16 13:04:09 +0900

    bj_1208 solve

--- Binary&Trinary Search/bj_1208.py
+++ Binary&Trinary Search/bj_1208.py
@@ -11,9 +11,9 @@ mid=len(input_)//2
 l1=input_[:mid]
 l2=input_[mid:]
 
-def left_sum(li):
+def get_sum(li):
     
-    res = {0: 0}
+    res = {0: 1}
 
     for num in li:
         # 현재까지의 결과를 복사하여 루프 도중 변경 방지
@@ -21,35 +21,27 @@ def left_sum(li):
         for partial_sum, count in current_items:
             new_sum = partial_sum + num
             # dict.get()이나 'in' 키워드는 O(1)의 속도를 가짐
-            res[new_sum] = res.get(new_sum, 0) + 1
+            res[new_sum] = res.get(new_sum, 0) + count
     
-    if res[0]==0:
-        del res[0]
+
     return res
 
-def right_sum(li,left_sum_list):
-    
-    res = [0]
-    count=0
-    for num in li:
-        # 현재까지의 결과를 복사하여 루프 도중 변경 방지
-        size=len(res)
-        for i in range(size):
-            new_sum = res[i] + num
-            res.append(new_sum)
-            # print('new:',new_sum)
-            # dict.get()이나 'in' 키워드는 O(1)의 속도를 가짐
-            if S-new_sum in left_sum_list:
-                count+=left_sum_list[S-new_sum]
-            if S==new_sum:
-                count+=1
-    
-  
-    return count
 
-left_sum_list=left_sum(l1)
-# print(left_sum_list)
-result=right_sum(l2,left_sum_list)
-if S in left_sum_list:
-    result+=left_sum_list[S]
-print(result)
+
+left_sum=get_sum(l1)
+
+right_sum=get_sum(l2)
+
+count=0
+
+
+for k,v in left_sum.items():
+    find=S-k
+    
+    if find in right_sum:
+        count+=right_sum[find]*v
+
+if S==0:
+    count-=1  ## 둘 다 공집합인 경우 제외
+
+print(count)
